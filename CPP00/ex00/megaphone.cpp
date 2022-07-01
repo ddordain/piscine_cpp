d256dfc2f15c33dcedc407c74f38b180f8c0827c
Dominique DORDAIN <ddordain@e2r1p7.42.fr>
2022-07-01 11:06:15 +0200

    update convert

--- CPP00/ex00/megaphone.cpp
+++ CPP00/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: ddordain <ddordain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/25 19:27:25 by ddordain          #+#    #+#             */
-/*   Updated: 2022/06/01 15:10:28 by ddordain         ###   ########.fr       */
+/*   Updated: 2022/07/01 11:05:39 by ddordain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,8 @@
 
 void    print_str_uppercase(char *str)
 {
-    for (int i = 0; i < (int)strlen(str); i++) {
-        std::cout << (char)toupper(str[i]);
+    for (int i = 0; i < static_cast<int>(std::strlen(str)); i++) {
+        std::cout << static_cast<char>(std::toupper(str[i]));
     }
 }
 
